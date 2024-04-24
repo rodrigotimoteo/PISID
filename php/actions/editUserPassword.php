41723eb0635c331e223a2d77346fe4cba981103f
AmaxSingh03 <162818376+AmaxSingh03@users.noreply.github.com>
2024-04-24 14:49:39 +0100

    action to change pass

---
+++ php/actions/editUserPassword.php
@@ -0,0 +1,80 @@
+<?php
+
+include("../config.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $sqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+
+    if ($sqli->connect_error) {
+        die("Connection failed: " . $sqli->connect_error);
+    }
+
+    if(isset($_SESSION['email'])) {
+
+        if(isset($_POST['new_password']) && isset($_POST['current_password'])) {
+
+            $current_password = $_POST['current_password'];
+            $new_password = $_POST['new_password'];
+            $email = $_SESSION['email'];
+
+            // Verify if current password is correct
+            $stmt_check_password = $sqli->prepare("SELECT password FROM utilizador WHERE email = ?");
+            $stmt_check_password->bind_param("s", $email);
+            $stmt_check_password->execute();
+            $stmt_check_password->store_result();
+
+            if($stmt_check_password->num_rows > 0) {
+                $stmt_check_password->bind_result($hashed_password);
+                $stmt_check_password->fetch();
+
+                // Verify if current password matches the stored password
+                if($current_password==$hashed_password) {
+                    // Passwords match, proceed with updating the password
+                    $procedure = "CALL EditUser(?,?,?,?)";
+                    $stmt = $sqli->prepare($procedure);
+
+                    $query = "SELECT telefone FROM utilizador WHERE email = ?";
+                    $stmt_get_phone = $sqli->prepare($query);
+                    $stmt_get_phone->bind_param("s", $_SESSION['email']);
+                    $stmt_get_phone->execute();
+                    $stmt_get_phone->bind_result($telefone);
+                    $stmt_get_phone->fetch();
+
+                    $stmt_get_phone->close();
+                    $hashed_new_password = $new_password;
+
+                    $stmt->bind_param("ssss",  $hashed_new_password, $_SESSION['name'], $telefone, $_SESSION['email']);
+
+
+                    if ($stmt->execute()) {
+
+                        echo "Password changed successfully";
+
+                    } else {
+
+                        echo "Error: " . $procedure . "<br>" . $sqli->error;
+
+                    }
+
+                    $stmt->close();
+
+
+                } else {
+                    echo "Current password is incorrect. " . $current_password;
+                }
+            } else {
+                echo "User not found.";
+            }
+
+            $stmt_check_password->close();
+        } else {
+            echo "Please provide both current and new passwords.";
+        }
+
+    }
+
+    $sqli->close();
+
+}
+?>
--- php/loginPage.php
+++ php/loginPage.php
@@ -5,227 +5,136 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
     echo "User already logged in, redirecting to test list";
     header("Location: testList.php");
 }
-
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>LoginPagePISID</title>
     <style>
-        @import url(https://fonts.googleapis.com/css?family=Rokkitt);
-@import url(https://fonts.googleapis.com/css?family=Open+Sans:400italic,400,700);
-@import url(https://fonts.googleapis.com/css?family=Roboto+Slab:700,400);
-@import url(https://fonts.googleapis.com/css?family=Lobster);
+        @import url('https://fonts.googleapis.com/css?family=Rokkitt|Open+Sans:400italic,400,700|Roboto+Slab:700,400|Lobster');
 
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
 
-* { margin: 0; padding: 0; -moz-box-sizing: border-box; box-sizing: border-box; }
+        body {
+            background: url('https://unsplash.s3.amazonaws.com/batch%206/park-place.jpg') no-repeat center center fixed;
+            background-size: cover;
+            margin: 0;
+            font-family: 'Open Sans', sans-serif;
+        }
 
-h1 { font-family: 'Rokkitt', serif; }
+        .container {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 50px;
+        }
 
-body {
-  background: url('https://unsplash.s3.amazonaws.com/batch%206/park-place.jpg') no-repeat center center fixed;
-  background-size: cover;
-  margin: 0 auto;
-  width: 66%;
-  font-family: 'Open Sans', sans-serif;
-}
+        .login {
+            flex: 1;
+            background-color: #444444;
+            padding: 30px;
+            border-radius: 10px;
+            box-shadow: 0 0 20px #000;
+        }
 
+        .login h1 {
+            color: #A64141;
+            font-size: 36px;
+            padding-bottom: 20px;
+            border-bottom: 2px solid #d25555;
+            margin-bottom: 20px;
+        }
 
-.container {
-  width: 66%;
-  min-width: 800px;
-  position: absolute;
-  top: 20%;
-}
-.span-6 {
-  float: left;
-  width: 49%;
-  margin-right: 1%;
-}
+        .login a {
+            display: block;
+            margin-bottom: 10px;
+            text-decoration: none;
+            color: #ccc;
+            font-size: 16px;
+        }
 
-.login {
-  background-color: #444444;
-  text-align: center;
-  box-shadow: 0 0 20px #000;
-}
-.login h1 {
-  color: #A64141;
-  font-size: 36px;
-  padding: 30px;
-  background-color: #d25555;
-}
-.login-content, .message {
-  padding: 30px 30px 50px 30px;
-}
-.login-content a {
-  text-decoration: none;
-  font-size: 14px;
-  color: #ccc;
-}
-label {
-  display: block;
-  color: #f4f4f4;
-  font-size: 18px;
-  margin-bottom: 3px;
-  opacity: 0;
-  
-  transition: all .3s ease-in-out;
-}
-input {
-  margin-bottom: 15px;
-  padding: 10px 5px;
-  width: 80%;
-  background-color: #ccc;
-  border: none;
-  text-align: center;
-  font-size: 16px;
-  font-family: 'Open Sans', sans-serif;
-  font-weight: bold;
-  color: #333;
-}
-input:focus {
-  background-color: #f4f4f4;
-  border: none;
-  outline: none;
-}
+        .message {
+            flex: 1;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            color: white;
+            font-size: 10em;
+            font-family: 'Lobster', cursive;
+            text-shadow: 3px 3px 0 #999;
+        }
 
-input[type=submit] {
-  color: #fff;
-  background-color: #d25555;
-  border: none;
-  font-family: 'Rokkitt', serif;
-  font-size: 24px;
-  margin-top: 25px;
-  width: 60%;
-  
-  transition: all .5s ease;
-}
-input[type=submit]:hover {
-  background-color: #A64141;
-  cursor: pointer;
-  color: #f4f4f4;
-}
+        form {
+            display: none;
+            flex-direction: column;
+            align-items: center;
+        }
 
-.message {
-  
-}
-.message span {
-  display: block;
-  color: rgba(255, 255, 255, 1);
-  position: relative; 
-  bottom: 80px;
-  text-align: center;
-}
-.line {
-  display: inline-block;
-  padding: 2px;
-  background-color: #fff;
-  -webkit-transform: rotate(-12deg);
-  -moz-transform: rotate(-12deg);
-  transform: rotate(-12deg);
-  width: 5px;
-}
-.first, .second, .third {
-  font-size:10em;
-  -webkit-transform: rotate(-12deg);
-  -moz-transform: rotate(-12deg);
-  transform: rotate(-12deg);
-  letter-spacing: -4px;
-}
-.first {
-  font-family: 'Lobster', cursive;
-  -webkit-text-shadow: 3px 3px 0 #999;
-  -moz-text-shadow: 3px 3px 0 #999;
-  text-shadow: 3px 3px 0 #999;
-}
-.second:before {
-  content: "";
-  display: block;
-  padding: 1px; 
-  background-color: #fff;
-  width: 170px;
-  position: absolute;
-  top: 50%;
-  right: 110px;
-}
-.second:after {
-  content: "";
-  display: block;
-  padding: 1px; 
-  background-color: #fff;
-  width: 170px;
-  position: absolute;
-  top: 50%;
-  left: 110px;
-}
-.secondS {
-  font-size: 8em;
-  font-family: 'Lobster', cursive;
-  -webkit-text-shadow: 3px 3px 0 #999;
-  -moz-text-shadow: 3px 3px 0 #999;
-  text-shadow: 3px 3px 0 #999;
-  line-height: 150px;
-}
-.show {
-  opacity: 1;
-}
+        label {
+            color: #f4f4f4;
+            font-size: 18px;
+            margin-bottom: 10px;
+        }
+
+        input {
+            margin-bottom: 20px;
+            padding: 10px 15px;
+            width: 300px;
+            background-color: #ccc;
+            border: none;
+            text-align: center;
+            font-size: 16px;
+            font-family: 'Open Sans', sans-serif;
+            color: #333;
+            border-radius: 5px;
+        }
+
+        input[type=submit] {
+            color: #fff;
+            background-color: #d25555;
+            border: none;
+            font-family: 'Rokkitt', serif;
+            font-size: 24px;
+            width: 60%;
+            cursor: pointer;
+            transition: background-color 0.3s ease;
+        }
+
+        input[type=submit]:hover {
+            background-color: #A64141;
+        }
     </style>
-    <script>
-       $("#email").focus(function(){
-    $("#emailLabel").addClass("show");
-    $(this).val('')
-}).blur(function(){
-    $("#emailLabel").removeClass("show");
-});
-
-$("#password").focus(function(){
-    $("#passwordLabel").addClass("show");
-    $(this).val('')
-}).blur(function(){
-    $("#passwordLabel").removeClass("show");
-});
-    </script>
 </head>
-<body>
-<header>
-<section class="container">
-        <div class="span-6">
-            <div class="login">
-                <h1>Greetings!</h1>
-                    <a href="../landingPage.php">Home</a>
-                </li>
-                <li>
-                    <a href="#" id="loginBtn">Login</a>
-                </li>
-            </ul>
-        </nav>
-    </div>
-</header>
 
-<div>
-    <div id="loginPopup">
-        <div id="loginForm">
+<body>
+<div class="container">
+    <div class="login">
+        <h1>Greetings!</h1>
+        <a href="../landingPage.php">Home</a>
+        <a href="#" id="loginBtn">Login</a>
+        <div id="loginPopup">
             <form action="actions/login.php" method="post">
-                <label id="emailLabel" for="email">Email:</label>
+                <label for="email">Email:</label>
                 <input type="text" required="" id="email" name="email">
-                <label id="passwordLabel" for="password">Password:</label>
+                <label for="password">Password:</label>
                 <input type="password" required="" id="password" name="password">
-                <a href="#">Forgot Username </a>&#x2022<a href="#"> Forgot Password</a><br>
-                <input type="submit" value="login" name="login">
+                <a href="#">Forgot Username </a>&#x2022;<a href="#"> Forgot Password</a><br>
+                <input type="submit" value="Login" name="login">
             </form>
         </div>
     </div>
-    <div class="span-6">
-            <div class="message">
-                <span class="first">Lab</span>
-                <span class="second">Rats</span>
-            </div>
-        </div>
+    <div class="message">
+        <span class="first">Lab</span>
+        <span class="second">Rats</span>
+    </div>
 </div>
-
 </body>
+
 </html>
-
-

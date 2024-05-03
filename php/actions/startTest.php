519e274cb510842329211164bfd1996da5f84cd4
tomasoli2002 <tomasoliveira2002@gmail.com>
2024-05-03 16:20:12 +0100

    starttest feito
    oi oi

---
+++ php/actions/startTest.php
@@ -0,0 +1,39 @@
+<?php
+include("../config.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+
+    $sqli = new mysqli(DB_SERVER, $_SESSION['email'], $_SESSION['password'], DB_DATABASE);
+
+    if ($sqli->connect_error) {
+        die("Connection failed: " . $sqli->connect_error);
+    }
+
+    if(isset($_SESSION['email'])) {
+
+        if(isset($_GET['test_id'])) {
+
+            $test_id = $_GET['test_id'];
+
+            $procedure = "CALL StartTest(?)";
+            $stmt = $sqli->prepare($procedure);
+            $stmt->bind_param("i", $test_id);
+
+            if ($stmt->execute()) {
+                header("Location: ../testList.php");
+            } else {
+                echo "Error: " . $procedure . "<br>" . $sqli->error;
+            }
+
+            $stmt->close();
+
+        } else {
+            echo "Test ID is not set.";
+        }
+
+    }
+
+    $sqli->close();
+
+}
+?>
